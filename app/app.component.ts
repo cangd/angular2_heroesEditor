fe8452368a683a40b6723fad3bad9d7534169a1a
Cang Dinh <cang.dinh@movingimage.com>
2016-05-15 22:07:19 +0200

    Routing routing routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component ({
     selector: 'my-app'
     template `
     <h1>{{title}}</h1>
-    <a [routerLink]="['Heroes']">Heroes</a>
+    <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
+    <hero-detail [hero]="selectedHero"></hero-detail>
     `,
     directives: [ROUTER_DIRECTIVES],
     providers: [
@@ -22,6 +28,17 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
         path: '/heroes',
         name: 'Heroes',
         component: HeroesComponent
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
     }
 ])
 
